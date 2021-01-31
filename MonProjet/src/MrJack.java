366af07698c408decfcc6bae7c4c0c6bcc39c9b2
ragh61641 <75576437+ragh61641@users.noreply.github.com>
2021-01-31 22:07:50 +0100

    Update MrJack.java

--- MonProjet/src/MrJack.java
+++ MonProjet/src/MrJack.java
@@ -4,7 +4,7 @@ public class MrJack extends Player {
     private PersonnagePlateau personnagePlateau;/// carte alibi pour chercher son identite?
     
     public MrJack(String name, String role, int nbSabliers, PersonnagePlateau personnagePlateau) {
-        super(name,role);
+        super(name);
         this.nbSabliers = 0;
         this.personnagePlateau = personnagePlateau;
     }
@@ -24,4 +24,11 @@ public class MrJack extends Player {
     public void setPersonnagePlateau(PersonnagePlateau personnagePlateau) {
         this.personnagePlateau = personnagePlateau;
     }
+    public void displayIdentiteMrJack() {
+		System.out.println( this.personnagePlateau .getNom() + " et " + nbSabliers);
+	}
+
+   
+	
+	
 }
